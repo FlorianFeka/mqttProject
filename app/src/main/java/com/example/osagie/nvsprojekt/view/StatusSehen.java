95bb850418074360beb77154ca20935cce26cf70
Jonathan OSAGIE <osa16304@spengergasse.at>
2018-02-10 21:41:55 +0100

    Basic status layout added(nichts verbunden)

---
+++ app/src/main/java/com/example/osagie/nvsprojekt/view/StatusSehen.java
@@ -0,0 +1,19 @@
+package com.example.osagie.nvsprojekt.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.osagie.nvsprojekt.R;
+
+/**
+ * Created by Jonathan on 10.02.2018.
+ */
+
+public class StatusSehen extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.status_sehen);
+
+    }
+}
